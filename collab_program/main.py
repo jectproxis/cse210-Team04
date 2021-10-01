8ec1ae5c209bab7de09651c3a51382781242f107
jectproxis <project634@gmail.com>
2021-10-01 14:59:06 -0600

    Adding methods to main

--- collab_program/main.py
+++ collab_program/main.py
@@ -1,9 +1,15 @@
 from collab_program.jackson import display_jeremy
+from collab_program.osguthorpe import sarah_intro
+from collab_program.penman import aiden_intro
 import jackson
+import osguthorpe
+import penman
 
 def main():
   print("Welcome to our collaborative program.")
   display_jeremy()
+  sarah_intro()
+  aiden_intro()
   
 if __name__ == "__main__":
     main()
